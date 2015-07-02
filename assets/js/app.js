03cecc376e7e0bb37b7316fbd45cef14d8c501f7
Bennie Kingwood <bennie.kingwood@gmail.com>
2015-07-02 11:23:11 -0400

    tooltip init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 /* global $,document,console,Parse */
 $(document).ready(function () {
+    
+    // init tooltip
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+    
      $('a[id^="navlink"]').hover(
         function (el) {
             if (el.target.id == 'navlink-stay-in-touch') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Stay in Touch</strong></a>');
