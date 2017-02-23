d85f06fe00d5e04c28c37a5ba285227af15fba2e
Bennie Kingwood <bennie.kingwood@gmail.com>
2017-02-23 12:43:15 -0800

    updated contact form.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,47 @@
 /* global $,document,console,Parse */
 
-$(document).ready(function () {
+$(document).ready(function() {
     // init tooltip
     // $(function () {
     //   $('[data-toggle="tooltip"]').tooltip();
     // });
-    
-     $('a[id^="navlink"]').hover(
-        function (el) {
+
+    $('a[id^="navlink"]').hover(
+        function(el) {
             if (el.target.id == 'navlink-stay-in-touch') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Stay in Touch</strong></a>');
             else if (el.target.id == 'navlink-work') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Work</strong></a>');
             else if (el.target.id == 'navlink-submit-project') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Submit Your Project</strong></a>');
             else if (el.target.id == 'navlink-home') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Home</strong></a>');
             else if (el.target.id == 'navlink-meet-team') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Meet the Team</strong></a>');
             else if (el.target.id == 'navlink-case-studies') $('#navlink-text').html('<a href="#" class="navlink-text"><strong>Case Studies</strong></a>');
-        }, function () {
+        },
+        function() {
             $('#navlink-text').html('');
         }
     );
-    
+
     $("button.close").on("click", function(e) {
         e.preventDefault();
-        $( this ).parent().hide();
+        $(this).parent().hide();
     });
     // form validation
-    var validForm = function () {
+    var validForm = function() {
         var retVal = true;
         var name = $('#name').val();
         var email = $('#email').val();
         var project = $('#project').val();
 
         // validate the name, email, and product fields
-        if (name === undefined || name.trim() === "" || 
-           email === undefined || email.trim() === "" ||
-           project === undefined || project.trim() === "") {
+        if (name === undefined || name.trim() === "" ||
+            email === undefined || email.trim() === "" ||
+            project === undefined || project.trim() === "") {
             retVal = false;
         }
         return retVal;
     };
 
     try {
-        var parseAPPID = "bcGAqn3tKfcGQ0POnXPTHetYjZcwcUhWbb3c5SHZ";
-        var parseJSID = "e6saZzL9M96uwxJ0oxr8zdFeHmrRWSoStjQ5WRhg";
-
-        Parse.initialize(parseAPPID, parseJSID);
-        var CommentObject = Parse.Object.extend("Comment");
-
-        $("#contactForm").on("submit", function (e) {
+        $("#contactForm").on("submit", function(e) {
             e.preventDefault();
             //add error handling here
             if (!validForm()) {
@@ -56,6 +51,10 @@ $(document).ready(function () {
                 $("#alert-error").hide();
             }
 
+            // disabled submit button + change text to "Please wait"
+            $("#submit-btn").prop("disabled", true);
+            $("#submit-btn").text("Please Wait");
+
             //gather the form data
             var data = {};
             data.name = $.sanitize($("#name").val().trim());
@@ -64,27 +63,42 @@ $(document).ready(function () {
             data.project = $.sanitize($("#project").val().trim());
             data.help = $.sanitize($("#help").val().trim());
 
-            var comment = new CommentObject();
-            comment.save(data, {
-                success: function () {
+            // submit the form data
+            $.ajax({
+                type: 'POST',
+                url: 'https://ezyflxh7ac.execute-api.us-west-2.amazonaws.com/prod/contact',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function() {
+                    // clear form and show a success message
                     $("#alert-success").show();
                     $('html,body').animate({
-                    scrollTop: 0
-                }, 500);
+                        scrollTop: 0
+                    }, 500);
+                    $("#contactForm").closest('form').find("input[type=text],input[type=email], textarea").val("");
+
+                    // disabled submit button + change text to "Submit Project"
+                    $("#submit-btn").prop("disabled", false);
+                    $("#submit-btn").text("Submit Project");
                 },
-                error: function (e) {
-                    console.log(JSON.stringify(e));
+                error: function() {
+                    // show an error message
+                    $("#alert-error-email").show();
+                    // disabled submit button + change text to "Submit Project"
+                    $("#submit-btn").prop("disabled", false);
+                    $("#submit-btn").text("Submit Project");
                 }
             });
         });
-    } catch(error) {} // digest 
+    } catch (error) {} // digest 
 });
 
 /**
  * This function will perform input sanitization function for the application
  */
-(function ($) {
-    $.sanitize = function (input) {
+(function($) {
+    $.sanitize = function(input) {
         var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
         replace(/<[\/\!]*?[^<>]*?>/gi, '').
         replace(/<style[^>]*?>.*?<\/style>/gi, '').
@@ -93,8 +107,8 @@ $(document).ready(function () {
     };
 })(jQuery);
 
- $(function () {
-    $('a[href*=#]:not([href=#])').click(function () {
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
